b7aff8700310ac14241de4ac42bf9373d4b09ff1
Pauletta <MPauletta@sft.ent.citco.com>
2019-02-07 11:56:28 -0400

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,42 +11,42 @@ export const environment = {
   mode: 'Development',
   production: false,
   isDebugMode: true,
-  authUrl: 'http://localhost:3000/api/auth/',
-  usersUrl: 'http://localhost:3000/api/users/',
-  groupsUrl: 'http://localhost:3000/api/groups/',
-  productsUrl: 'http://localhost:3000/api/products/',
-  membersUrl: 'http://localhost:3000/api/members/',
-  friendsUrl: 'http://localhost:3000/api/friends/',
-  photosUrl: 'http://localhost:3000/api/photos/',
-  videosUrl: 'http://localhost:3000/api/videos/',
-  docsUrl: 'http://localhost:3000/api/documents/',
-  eventsUrl: 'http://localhost:3000/api/events/',
-  mailsUrl: 'http://localhost:3000/api/mails/',
-  mailactionsUrl: 'http://localhost:3000/api/mailactions/',
-  notificationsUrl: 'http://localhost:3000/api/notifications/',
-  auctionsUrl: 'http://localhost:3000/api/auctions/',
-  productphotosUrl: 'http://localhost:3000/api/productphotos/',
-  forumsUrl: 'http://localhost:3000/api/forums/',
-  groupnotificationsUrl: 'http://localhost:3000/api/groupnotifications/',
-  chatsUrl: 'http://localhost:3000/api/chats/',
-  forumpostsUrl: 'http://localhost:3000/api/forumposts/',
-  forumpostactionsUrl: 'http://localhost:3000/api/forumpostactions/',
-  groupmembersUrl: 'http://localhost:3000/api/groupmembers/',
-  notesUrl: 'http://localhost:3000/api/notes/',
-  groupnotesUrl: 'http://localhost:3000/api/groupnotes/',
-  foldersUrl: 'http://localhost:3000/api/folders/',
-  groupfoldersUrl: 'http://localhost:3000/api/groupfolders/',
-  postsUrl: 'http://localhost:3000/api/posts/',
-  grouppostsUrl: 'http://localhost:3000/api/groupposts/',
-  likesUrl: 'http://localhost:3000/api/likes/',
-  commentsUrl: 'http://localhost:3000/api/comments/',
-  addressesUrl: 'http://localhost:3000/api/addresses/',
-  userPrivacyUrl: 'http://localhost:3000/api/privacy/',
-  userSecurityUrl: 'http://localhost:3000/api/security/',
-  watchsUrl: 'http://localhost:3000/api/watchs/',
-  popularisimoUrl: 'http://localhost:90/popularisimo',
-  socketserverurl: 'http://localhost:4001/'
-};
+  authUrl: 'http://www.popularisimo.com:3000/api/auth/',
+  usersUrl: 'http://www.popularisimo.com:3000/api/users/',
+  groupsUrl: 'http://www.popularisimo.com:3000/api/groups/',
+  productsUrl: 'http://www.popularisimo.com:3000/api/products/',
+  membersUrl: 'http://www.popularisimo.com:3000/api/members/',
+  friendsUrl: 'http://www.popularisimo.com:3000/api/friends/',
+  photosUrl: 'http://www.popularisimo.com:3000/api/photos/',
+  videosUrl: 'http://www.popularisimo.com:3000/api/videos/',
+  docsUrl: 'http://www.popularisimo.com:3000/api/documents/',
+  eventsUrl: 'http://www.popularisimo.com:3000/api/events/',
+  mailsUrl: 'http://www.popularisimo.com:3000/api/mails/',
+  mailactionsUrl: 'http://www.popularisimo.com:3000/api/mailactions/',
+  notificationsUrl: 'http://www.popularisimo.com:3000/api/notifications/',
+  auctionsUrl: 'http://www.popularisimo.com:3000/api/auctions/',
+  productphotosUrl: 'http://www.popularisimo.com:3000/api/productphotos/',
+  forumsUrl: 'http://www.popularisimo.com:3000/api/forums/',
+  groupnotificationsUrl: 'http://www.popularisimo.com:3000/api/groupnotifications/',
+  chatsUrl: 'http://www.popularisimo.com:3000/api/chats/',
+  forumpostsUrl: 'http://www.popularisimo.com:3000/api/forumposts/',
+  forumpostactionsUrl: 'http://www.popularisimo.com:3000/api/forumpostactions/',
+  groupmembersUrl: 'http://www.popularisimo.com:3000/api/groupmembers/',
+  notesUrl: 'http://www.popularisimo.com:3000/api/notes/',
+  groupnotesUrl: 'http://www.popularisimo.com:3000/api/groupnotes/',
+  foldersUrl: 'http://www.popularisimo.com:3000/api/folders/',
+  groupfoldersUrl: 'http://www.popularisimo.com:3000/api/groupfolders/',
+  postsUrl: 'http://www.popularisimo.com:3000/api/postsUrl/',
+  grouppostsUrl: 'http://www.popularisimo.com:3000/api/groupposts/',
+  likesUrl: 'http://www.popularisimo.com:3000/api/likes/',
+  commentsUrl: 'http://www.popularisimo.com:3000/api/comments/',
+  addressesUrl: 'http://www.popularisimo.com:3000/api/addresses/',
+  userPrivacyUrl: 'http://www.popularisimo.com:3000/api/privacy/',
+  userSecurityUrl: 'http://www.popularisimo.com:3000/api/security/',
+  watchsUrl: 'http://www.popularisimo.com:3000/api/watchs/',
+  popularisimoUrl: 'http://www.popularisimo.com/popularisimo',
+  socketserverurl: 'http://www.popularisimo.com:4001/'
+};
 
 
 
